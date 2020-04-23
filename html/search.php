847620d4c195d496d12cc9ce5932a9e71996a887
David Slater <dslater@illinois.edu>
2020-04-23 10:39:37 -0500

    migrated search.php to twig

--- html/search.php
+++ html/search.php
@@ -1,71 +1,22 @@
-<?php require_once 'includes/header.inc.php'; ?>
+<?php require_once 'includes/header.inc.php'; 
 
-    <main role="main">
-        <div class="container">
-            <h1 class="text-center">Search</h1>
-        </div>
+$twig_search_variables = array();
 
-        <div class="container" id="searchUi">
-            <div>
-                <form>
-                <h3>Find by UniProt ID</h3>
-                <p>
-                    Input a UniProt ID to identify the associated cluster within the 
-                    radical SAM superfamily.
-                </p>
-                <div id="searchIdErrorMsg" style="display: none" class="m-1 p-2 mb-3 rounded alert-warning search-error"></div>
-                <div class="form-group">
-                    <input class="form-control w-25" type="text" name="searchId" id="searchId" />
-                </div>
-                <div class="form-group mb-5 mt-1"><button class="btn btn-primary btn-sm" id="searchIdBtn">Find Cluster</button></div>
-            </form>
-            </div>
+$twig_search_loader = new \Twig\Loader\FilesystemLoader(settings::get_twig_dir());
+$twig_search = new \Twig\Environment($twig_search_loader);
 
-            <div>
-                <h3>Find by Sequence</h3>
-                <p>
-                    Input a sequence to find a related cluster within the
-                    radical SAM superfamily.  The cluster that most closely matches the input sequence
-                    will be returned, allowing the user to access the associated data, including ID list,
-                    HMM, WebLogo, and other files.  The search is done with hmmscan from the HMMER package,
-                    against a database of HMMs collected from the clusters in the radical SAM superfamily.
-                </p>
-                <div id="searchSeqErrorMsg" style="display: none" class="m-1 p-2 mb-3 rounded alert-warning search-error"></div>
-                <div class="form-group">
-                    <textarea class="form-control w-75" id="searchSeq" cols="70" rows="10"></textarea>
-                </div>
-                <div class="mt-1"><button class="btn btn-primary btn-sm" id="searchSeqBtn">Find Clusters</button></div>
-            </div>
+$search_html = "";
+if (file_exists(settings::get_twig_dir() . "/custom/search.html.twig")) {
+        $search_html = $twig_search->render("custom/search.html.twig",$twig_search_variables);
+}
+else {
+        $search_html = $twig_search->render("default/search.html.twig",$twig_search_variables);
+}
 
-            <div>
-                <h3>Find by Taxonomy</h3>
-                <p>
-                </p>
-                <div id="searchTaxTermErrorMsg" style="display: none" class="m-1 p-2 mb-3 rounded alert-warning search-error"></div>
-                <div class="form-group" style="display: none">
-                    <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="searchTaxType" id="searchTaxTypeSpecies" value="1" checked>
-                        <label class="form-check-label" for="searchTaxTypeSpecies">Species</label>
-                    </div>
-                    <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="searchTaxType" id="searchTaxTypeGenus" value="1">
-                        <label class="form-check-label" for="searchTaxTypeGenus">Genus</label>
-                    </div>
-                    <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="searchTaxType" id="searchTaxTypeFamily" value="1">
-                        <label class="form-check-label" for="searchTaxTypeFamily">Family</label>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <input class="form-control w-50" type="text" name="searchTaxTerm" id="searchTaxTerm" placeholder="Species" />
-                </div>
-                <div class="mt-1"><button class="btn btn-primary btn-sm" id="searchTaxTermBtn">Find Clusters</button></div>
-            </div>
-        </div>
+echo $search_html;
 
-        <div class="container" id="searchResults" style="display: none">
-        </div>
-    </main>
 
-<?php require_once 'includes/footer.inc.php'; ?>
+require_once 'includes/footer.inc.php'; 
+
+?>
 
