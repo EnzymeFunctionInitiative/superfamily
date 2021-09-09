553e8ae814e6ed0fa3422e4c0aa098b915ae2137
Nils Oberg <noberg@illinois.edu>
2021-09-09 15:14:39 -0500

    Added tutorials page

---
+++ html/tutorials.php
@@ -0,0 +1,11 @@
+<?php
+require_once(__DIR__ . "/../init.php");
+
+$twig_variables = array();
+$loader = new \Twig\Loader\FilesystemLoader(settings::get_twig_dir());
+$twig = new \Twig\Environment($loader);
+
+$html = $twig->render("tutorials.html.twig", $twig_variables);
+
+echo $html;
+
